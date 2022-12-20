1fa7e2bdf8aefe5ad243af276a5d936305c756bd
tzviki <twilhelm@votis.net>
2022-12-20 22:10:52 +0200

    finish useState

--- tutorial/src/tutorial/1-useState/setup/1-error-example.js
+++ tutorial/src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'random title'
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+    </React.Fragment>
+  )
 };
 
 export default ErrorExample;
--- tutorial/src/tutorial/1-useState/setup/5-useState-counter.js
+++ tutorial/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value , setValue] = useState(0)
+  return <>
+    <section style={{margin: '4rem 0'}} >
+      <h2>regular counter</h2>
+      <h1>{value}</h1>
+      <button className="btn" onClick={()=>setValue(value - 1 )} >decrase</button>
+      <button className="btn" onClick={()=>setValue(0)} >reset</button>
+      <button className="btn" onClick={()=>setValue(value + 1 )} >incrase</button>
+    </section>
+  </>;
 };
 
 export default UseStateCounter;
